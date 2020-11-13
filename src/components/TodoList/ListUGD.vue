88d9e839a40c51f70260477145996a396c401ef7
Asus <vellin1999@gmail.com>
2020-11-13 09:32:59 +0700

    UGD Vue CLI

--- src/components/TodoList/ListUGD.vue
+++ src/components/TodoList/ListUGD.vue
@@ -11,27 +11,27 @@
 					single-line
 					hide-details
 				></v-text-field>
+
 				<v-spacer></v-spacer>
+
 				<v-btn class="mr-3" color="purple" dark @click="dialogFinished = true">
 					Todo Selesai
 				</v-btn>
+
 				<v-btn color="success" dark @click="dialog = true">
 					Tambah
 				</v-btn>
 			</v-card-title>
+
 			<v-data-table :headers="headers" :items="todos" :search="search">
 				<template v-slot:[`item.priority`]="{ item }">
-					<v-chip outlined label :color="getColor(item.priority)" dark>
-						{{ item.priority }}
-					</v-chip>
+					<v-chip outlined label :color="getColor(item.priority)" dark>{{ item.priority }} </v-chip>
 				</template>
+
 				<template v-slot:[`item.actions`]="{ item }">
-					<v-icon color="blue" small class="mr-2" @click="editItem(item)">
-						mdi-pencil
-					</v-icon>
-					<v-icon color="red" small @click="deleteItem(item)">
-						mdi-delete
-					</v-icon>
+					<v-icon color="blue" small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+
+					<v-icon color="red" small @click="deleteItem(item)">mdi-delete</v-icon>
 				</template>
 			</v-data-table>
 		</v-card>
@@ -41,28 +41,22 @@
 				<v-card-title>
 					<span class="headline">Form Todo</span>
 				</v-card-title>
+
 				<v-card-text>
 					<v-container>
 						<v-text-field v-model="formTodo.task" label="Task" required></v-text-field>
 
-						<v-select
-							v-model="formTodo.priority"
-							:items="['Penting', 'Biasa', 'Tidak penting']"
-							label="Priority"
-							required
-						></v-select>
+						<v-select v-model="formTodo.priority" :items="['Penting', 'Biasa', 'Tidak penting']" label="Priority" required></v-select>
 
 						<v-textarea v-model="formTodo.note" label="Note" required></v-textarea>
 					</v-container>
 				</v-card-text>
+
 				<v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn color="blue darken-1" text @click="cancel">
-						Cancel
-					</v-btn>
-					<v-btn color="blue darken-1" text @click="save">
-						Save
-					</v-btn>
+					<v-btn color="blue darken-1" text @click="cancel"> Cancel</v-btn>
+
+					<v-btn color="blue darken-1" text @click="save">Save</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-dialog>
@@ -72,15 +66,13 @@
 				<v-card-title>
 					<span class="headline font-weight-bold">Yakin ingin menghapus?</span>
 				</v-card-title>
+
 				<v-card-actions>
 					<v-spacer></v-spacer>
 					<v-spacer></v-spacer>
-					<v-btn color="green darken-1" text @click="cancelDelete">
-						Tidak
-					</v-btn>
-					<v-btn color="red darken-1" text @click="confirmDelete">
-						Ya
-					</v-btn>
+					<v-btn color="green darken-1" text @click="cancelDelete">Tidak </v-btn>
+
+					<v-btn color="red darken-1" text @click="confirmDelete">Ya</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-dialog>
@@ -90,27 +82,30 @@
 				<v-card-title>
 					<span class="headline font-weight-bold">Finished To do</span>
 				</v-card-title>
+
 				<v-card-text>
 					<v-container>
-						<v-data-table :headers="headersFinished" :items="finishedTodos">
+						<v-data-table :headers="finished" :items="finishedTodos">
+
 							<template v-slot:[`item.priority`]="{ item }">
-								<v-chip outlined label :color="getColor(item.priority)" dark>
-									{{ item.priority }}
-								</v-chip>
+								<v-chip outlined label :color="getColor(item.priority)" dark>{{ item.priority }}</v-chip>
 							</template>
+
 						</v-data-table>
 					</v-container>
 				</v-card-text>
+
 				<v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn color="blue darken-1" text @click="dialogFinished = false">
-						Close
-					</v-btn>
+
+					<v-btn color="blue darken-1" text @click="dialogFinished = false"> Close</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-dialog>
 	</v-main>
 </template>
+
+
 <script>
 export default {
 	name: "List",
@@ -132,7 +127,7 @@ export default {
 				{ text: "Note", value: "note" },
 				{ text: "Actions", value: "actions" },
 			],
-			headersFinished: [
+			finished: [
 				{
 					text: "Task",
 					align: "start",
